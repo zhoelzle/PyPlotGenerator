cb6d36c449a791d6da8c943b0146363a67da6e57
Zachary Hoelzle <zacharyhoelzle@Zacharys-MacBook-Air.local>
2024-02-10 22:42:34 -0500

    Updated a comment in main, deleted a plot

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ print(f"Unique Filename: {unique_filename}")
 # Save the plot to an image file within the notebook
 plt.savefig(unique_filename)
 
-#git@github.com:zhoelzle/PyPlotGenerator.git
+#new-feature test
